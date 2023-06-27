176a0d04a4b18140ea00fd40eeadf1566e68289a
Deepika <deepikardb@gmail.com>
2023-06-26 17:56:06 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UserDefaultsService
+
+A description of this package.
---
+++ Tests/UserDefaultsServiceTests/UserDefaultsServiceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UserDefaultsService
+
+final class UserDefaultsServiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UserDefaultsService().text, "Hello, World!")
+    }
+}
